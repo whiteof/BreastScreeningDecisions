5c52be546adae7cd72089a9f30b3290441b45969
Victor Yurkin <viy2003@pc171816.med.cornell.edu>
2016-11-17 16:27:43 -0500

    Your Risk tab implemented.

---
+++ BreastScreeningDecisions/ValuesViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ValuesViewController.swift
+//  BreastScreeningDecisions
+//
+//  Created by Victor Yurkin on 11/17/16.
+//  Copyright © 2016 Weill Cornell Medicine. All rights reserved.
+//
+
+import UIKit
+
+class ValuesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func resetApp(_ sender: Any) {
+        ApplicationDataModel.sharedInstance.removeUserData()
+        performSegue(withIdentifier: "unwindToIntro", sender: self)
+    }
+
+}
